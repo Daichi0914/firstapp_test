6ff60699785c12ab1c06ca767593f5f08968f719
Daichi0914 <goludrozya44@gmail.com>
2020-07-10 18:51:39 +0900

    Updates

---
+++ precache-manifest.1cbbf57d3526d9488d05645c84bdcede.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4447c722523d39e2199e5b2c4f0efa17",
+    "url": "/firstapp_test/index.html"
+  },
+  {
+    "revision": "d583568b29353972ca6d",
+    "url": "/firstapp_test/static/js/2.c94e99fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/firstapp_test/static/js/2.c94e99fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc909d18726e97e85340",
+    "url": "/firstapp_test/static/js/main.074869d7.chunk.js"
+  },
+  {
+    "revision": "2c320bb5a887b6f4bbd9",
+    "url": "/firstapp_test/static/js/runtime-main.7869ae4a.js"
+  }
+]);
